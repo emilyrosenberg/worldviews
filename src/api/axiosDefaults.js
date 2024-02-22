94780c03d26e90e3092398788c9d885c152ddf39
Emily Rosenberg <earosenberg@gmail.com>
2024-02-22 23:00:13 +0100

    Add access and refresh tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://worldviews-api-48c7a84b222a.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
