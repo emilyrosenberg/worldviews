e5841d0aed1f7fceefea1e4769f48d4a2ef15d8e
Emily Rosenberg <earosenberg@gmail.com>
2024-02-24 14:44:58 +0100

    Update heroku link

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://worldviews-api-48c7a84b222a.herokuapp.com";
+axios.defaults.baseURL = "https://worldviews-api1-2fa5e8a86642.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
