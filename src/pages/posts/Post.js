c55d2a9adbb2e8ad3abb2eb07f16d45df7f4df40
Emily Rosenberg <earosenberg@gmail.com>
2024-02-25 12:02:52 +0000

    Fix typos from last commit

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,3 +1,9 @@
+import React from "react";
+import styles from "../../styles/Post.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+import Avatar from "../../components/Avatar";
 import { axiosRes } from "../../api/axiosDefaults";
 import { MoreDropdown } from "../../components/MoreDropdown";
 
