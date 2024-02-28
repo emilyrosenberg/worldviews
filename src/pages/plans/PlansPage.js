30f104d4a0157f609254059dc4d673452a179448
Emily Rosenberg <earosenberg@gmail.com>
2024-02-28 21:23:49 +0000

    Add search bar for plans

--- src/pages/plans/PlansPage.js
+++ src/pages/plans/PlansPage.js
@@ -15,13 +15,15 @@ import NoResults from "../../assets/no-results.png"
 
 function PlansPage( { message, filter = "" }) {
   const [plans, setPlans] = useState({ results: [] });
-  const [hasLoaded, setHasLoaded] = useState();
+  const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
+
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     const fetchPlans = async () => {
       try {
-        const { data } = await axiosReq.get(`/plans/?${filter}`);
+        const { data } = await axiosReq.get(`/plans/?${filter}search=${query}`);
         setPlans(data);
         setHasLoaded(true);
       } catch (err) {
@@ -29,14 +31,36 @@ function PlansPage( { message, filter = "" }) {
       }
     };
 
-    setHasLoaded();
-    fetchPlans();
-  }, [filter, pathname]);
+    setHasLoaded(false);
+    const timer = setTimeout(() => {
+      fetchPlans();
+    }, 1000)
+    return () => {
+      clearTimeout(timer)
+    }
+
+    
+  }, [filter, query, pathname]);
    
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
+
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search plans"
+          />
+        </Form>
+
         {hasLoaded ? (
           <>
             {plans.results.length ? (
