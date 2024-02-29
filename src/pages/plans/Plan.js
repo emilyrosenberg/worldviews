4cf0c5c7cda05cd6d3ca3adf7296df726d5105b7
Emily Rosenberg <earosenberg@gmail.com>
2024-02-29 11:31:02 +0000

    Add edit and delete for plans

--- src/pages/plans/Plan.js
+++ src/pages/plans/Plan.js
@@ -4,6 +4,9 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Card, Media } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
+import { MoreDropdown } from "../../components/MoreDropdown";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { axiosRes } from "../../api/axiosDefaults";
 
 const Plan = (props) => {
   const {
@@ -20,6 +23,20 @@ const Plan = (props) => {
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner;
+  const history = useHistory();
+
+  const handleEdit = () => {
+    history.push(`/plans/$(id)/edit`)
+  };
+
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/plans/${id}/`);
+      history.goBack();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <Card className={styles.Plan}>
@@ -31,7 +48,12 @@ const Plan = (props) => {
           </Link>
           <div className="d-flex align-items-center">
             <span>{updated_at}</span>
-            {is_owner && planPage && "..."}
+            {is_owner && planPage && (
+              <MoreDropdown
+                handleEdit={handleEdit}
+                handleDelete={handleDelete}
+              />
+            )}
           </div>
         </Media>
       </Card.Body>
