779a575b304ae2e41c6721614c48c569f8fa6dd8
Emily Rosenberg <earosenberg@gmail.com>
2024-02-29 15:23:55 +0000

    Create plan_comment model

---
+++ src/pages/comments/PlanComment.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import Media from "react-bootstrap/Media";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
+import { MoreDropdown } from "../../components/MoreDropdown";
+// import CommentEditForm from "./CommentEditForm";
+
+import styles from "../../styles/Comment.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { axiosRes } from "../../api/axiosDefaults";
+
+const PlanComment = (props) => {
+  const {
+    profile_id,
+    profile_image,
+    owner,
+    updated_at,
+    content,
+    id,
+    setPlan,
+    setPlanComments,
+  } = props;
+
+  const [showPlanEditForm, setShowPlanEditForm] = useState(false);
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
+  const handleDelete = async () => {
+    try {
+      await axiosRes.delete(`/comments/${id}/`);
+      setPlan((prevPlan) => ({
+        results: [
+          {
+            ...prevPlan.results[0],
+            comments_count: prevPlan.results[0].comments_count - 1,
+          },
+        ],
+      }));
+
+      setPlanComments((prevPlanComments) => ({
+        ...prevPlanComments,
+        results: prevPlanComments.results.filter((plan_comment) => plan_comment.id !== id),
+      }));
+    } catch (err) {}
+  };
+
+  return (
+    <>
+      <hr />
+      <Media>
+        <Link to={`/profiles/${profile_id}`}>
+          <Avatar src={profile_image} />
+        </Link>
+        <Media.Body className="align-self-center ml-2">
+          <span className={styles.Owner}>{owner}</span>
+          <span className={styles.Date}>{updated_at}</span>
+          {showEditForm ? (
+            <PlanCommentEditForm
+              id={id}
+              profile_id={profile_id}
+              content={content}
+              profileImage={profile_image}
+              setComments={setComments}
+              setShowPlanEditForm={setShowPlanEditForm}
+            />
+          ) : (
+            <p>{content}</p>
+          )}
+        </Media.Body>
+        {is_owner && !showPlanEditForm && (
+          <MoreDropdown
+            handleEdit={() => setShowPlanEditForm(true)}
+            handleDelete={handleDelete}
+          />
+        )}
+      </Media>
+    </>
+  );
+};
+
+export default PlanComment;
--- src/pages/plans/PlanPage.js
+++ src/pages/plans/PlanPage.js
@@ -4,23 +4,36 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import Plan from "./Plan";
+import Comment from "../comments/Comment";
 
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 
+import PlanCommentCreateForm from "../comments/PlanCommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
+
 function PlanPage() {
   const { id } = useParams();
   const [plan, setPlan] = useState({ results: [] });
 
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setPlanComments] = useState({ results: [] });
+
   useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{ data: plan }] = await Promise.all([
+        const [{ data: plan }, { data: comments}] = await Promise.all([
           axiosReq.get(`/plans/${id}`),
+          axiosReq.get(`/comments/?plan=${id}`)
         ]);
         setPlan({ results: [plan] });
-        console.log(plan);
+        setPlanComments(comments);
       } catch (err) {
         console.log(err);
       }
@@ -34,7 +47,39 @@ function PlanPage() {
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
         <Plan {...plan.results[0]} setPlans={setPlan} planPage />
-        <Container className={appStyles.Content}>Comments</Container>
+        <Container className={appStyles.Content}>
+          {currentUser ? (
+            <PlanCommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              plan={id}
+              setPlan={setPlan}
+              setPlanComments={setPlanComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+          {comments.results.length ? (
+            <InfiniteScroll
+              children={comments.results.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  {...comment}
+                  setPlan={setPlan}
+                  setPlanComments={setPlanComments}
+                />
+              ))}
+              dataLength={comments.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!comments.next}
+              next={() => fetchMoreData(comments, setPlanComments)}
+            />
+          ) : currentUser ? (
+            <span>No comments yet, be the first to comment!</span>
+          ) : (
+            <span>No comments... yet</span>
+          )}
+        </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Popular profiles for desktop
