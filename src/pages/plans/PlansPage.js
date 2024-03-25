3f08edc987c2572bac123d051bdebe5203380242
Emily Rosenberg <earosenberg@gmail.com>
2024-03-25 18:19:29 +0000

    Add currentUser to Plans to test

--- src/pages/plans/PlansPage.js
+++ src/pages/plans/PlansPage.js
@@ -17,6 +17,7 @@ import NoResults from "../../assets/no-results.png";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 import PopularProfiles from "../profiles/PopularProfiles";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function PlansPage({ message, filter = "" }) {
   const [plans, setPlans] = useState({ results: [] });
@@ -24,6 +25,8 @@ function PlansPage({ message, filter = "" }) {
   const { pathname } = useLocation();
 
   const [query, setQuery] = useState("");
+
+  const currentUser = useCurrentUser();
 
   useEffect(() => {
     const fetchPlans = async () => {
@@ -43,7 +46,7 @@ function PlansPage({ message, filter = "" }) {
     return () => {
       clearTimeout(timer)
     } 
-  }, [filter, query, pathname]);
+  }, [filter, query, pathname, currentUser]);
    
   return (
     <Row className="h-100 mt-5">
